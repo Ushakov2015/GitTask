f2e5635395bb839504d7a02d484f9d17c5453519
Ushakov2015 <36144146+Ushakov2015@users.noreply.github.com>
2018-05-27 18:55:47 +0800

    Add files via upload
    Сложение

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Введите число a");
+    int a = in.nextInt();
+    System.out.println("Введите число b");
+    int b = in.nextInt();
+
+    System.out.println(Main.Sum(a,b12));
+
+    }
+
+    public static int Sum (int a, int b) {
+
+       int Sum = a+b;
+
+       return Sum;
+
+    }
+}
